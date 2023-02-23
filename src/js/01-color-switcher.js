09c94b9ceb12be43bae90cc711762861f3abe283
aliveness-zp <81018609+aliveness-zp@users.noreply.github.com>
2023-02-23 18:11:49 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
+const startBtn = document.querySelector('[data-start]');
+const stoptBtn = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
+let timerId = 0;
+stoptBtn.setAttribute('disabled', true);
 
+console.log(startBtn);
+console.log(bodyEl);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+// START_BTN
+
+startBtn.addEventListener('click', startBodyColorChangeInterval);
+
+function startBodyColorChangeInterval() {
+  timerId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+    console.log(bodyEl.style.backgroundColor);
+    startBtn.setAttribute('disabled', true);
+    stoptBtn.removeAttribute('disabled');
+  }, 1000);
+}
+
+// STOP_BTN
+
+stoptBtn.addEventListener('click', stopBodyColorChangeInterval);
+
+function stopBodyColorChangeInterval() {
+  clearInterval(timerId);
+  startBtn.removeAttribute('disabled');
+  stoptBtn.setAttribute('disabled', true);
+}
