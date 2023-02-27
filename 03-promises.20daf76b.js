4b192d52eaf15fcf597e1cdf26b74a3ac2ca13ee
aliveness-zp <81018609+aliveness-zp@users.noreply.github.com>
2023-02-27 08:50:31 +0000

    Deploying to gh-pages from @ aliveness-zp/goit-js-hw-09@cab1b720431b82db51092faa12e11d41f6273b6e 🚀

---
+++ 03-promises.20daf76b.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".form");e.addEventListener("submit",(function(e,n){var t=Math.random()>.3;return new Promise((function(u,o){setTimeout((function(){t?u({position:e,delay:n}):o({position:e,delay:n})}),n)}))}));Number(e.delay.value),Number(e.step.value),Number(e.amount.value)}();
+//# sourceMappingURL=03-promises.20daf76b.js.map
